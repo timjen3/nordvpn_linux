c5b12800b7a265721fdb021ade41cfba19ca91f2
Timothy Jannace <timjen3@gmail.com>
2017-07-15 18:51:54 -0400

    Ping should be in linux format.

--- tools/speed_test.py
+++ tools/speed_test.py
@@ -5,7 +5,7 @@ def get_avg_ping(host):
 	_num_pings = 5
 	total_response_time = 0
 	host = host.replace("https", "").replace("http", "").replace("://", "")
-	ping = subprocess.Popen(["ping", "-n", str(_num_pings), "-w", "1000", host], stdout=subprocess.PIPE)
+	ping = subprocess.Popen(["ping", "-c", str(_num_pings), host], stdout=subprocess.PIPE)
 	for line in ping.stdout.readlines():
 		line_as_string = line.decode("utf-8")
 		if "time=" in line_as_string and "ms" in line_as_string:
