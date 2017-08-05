ffcda4126ceffb33020f6edf3d3b33a2e530913f
Timothy Jannace <timjen3@gmail.com>
2017-08-05 19:30:08 -0400

    ...

--- tools/linux.py
+++ tools/linux.py
@@ -3,5 +3,5 @@ import os
 
 def send_desktop_msg(msg_string):
 	safe_string = msg_string.replace("\n", ";")
-	msg = "notify-send '{}'".format(safe_string)
+	msg = 'notify-send "{}"'.format(safe_string)
 	os.popen(msg)
