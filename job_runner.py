47c351fd30d748686d709d95022bde9c2e1a43d8
Timothy Jannace <timjen3@gmail.com>
2017-09-03 16:33:47 -0400

    refactoring

--- job_runner.py
+++ job_runner.py
@@ -5,21 +5,32 @@ from tools.threading import threaded
 from tools.localinfo import get_meta
 from tools.linux import pid_exists
 import logging
+import json
 import os
+__TOOL_CONFIG__ = None
 
 
+class Static:
+	def __init__(self, cls):
+		cls()
+
+
+@Static
 class JobRunner:
 	__TOOL_CONFIG__ = None
 
-	def __init__(self, config):
-		JobRunner.__TOOL_CONFIG__ = config
+	def __init__(self):
+		with open("content/tool.json", "r") as fp:
+			config = json.loads(fp.read())
+		global __TOOL_CONFIG__
+		__TOOL_CONFIG__ = config
 
 
 @threaded
 def connect_vpn():
 	os.environ["APPLICATION_ROOT"] = os.getcwd()
 	logger = logging.getLogger("NORDVPNLINUX")
-	SM = ServerManager(config=JobRunner.__TOOL_CONFIG__)
+	SM = ServerManager(config=__TOOL_CONFIG__)
 	for server in SM.servers:
 		response_time = get_avg_ping(server.ip)
 		target_server_stats = "\n\t".join([
@@ -31,13 +42,13 @@ def connect_vpn():
 			"Features: {}".format(",".join([k for k, v in server.features.items() if v])),  # List server features
 			"Categories: {}".format(server.categories),
 			"Search Keywords: {}".format(server.search_keywords),
-			"File: {}".format(openvpn_connector.get_ovpn_file_path(domain_name=server.domain, config=JobRunner.__TOOL_CONFIG__))
+			"File: {}".format(openvpn_connector.get_ovpn_file_path(domain_name=server.domain, config=__TOOL_CONFIG__))
 		])
-		if response_time >= JobRunner.__TOOL_CONFIG__["max_response_time"]:
+		if response_time >= __TOOL_CONFIG__["max_response_time"]:
 			logger.debug("Skipping a server because it is responding too slowly:\n\t" + target_server_stats)
 		else:
 			logger.info("Connecting to server via vpn tunnel...\n\t" + target_server_stats)
-			openvpn_connector.start_vpn_service(server.domain, JobRunner.__TOOL_CONFIG__, SM.locale_data)
+			openvpn_connector.start_vpn_service(server.domain, __TOOL_CONFIG__, SM.locale_data)
 			t = check_alive()
 			return t.result_queue.get()
 
@@ -53,8 +64,8 @@ def disconnect_function():
 def check_alive():
 	"""Checks if process defined in pid file is alive."""
 	is_alive = False
-	if "pid_file" in JobRunner.__TOOL_CONFIG__ and os.path.exists(JobRunner.__TOOL_CONFIG__["pid_file"]):
-		with open(JobRunner.__TOOL_CONFIG__["pid_file"], "r") as fp:
+	if "pid_file" in __TOOL_CONFIG__ and os.path.exists(__TOOL_CONFIG__["pid_file"]):
+		with open(__TOOL_CONFIG__["pid_file"], "r") as fp:
 			pid = fp.read().strip(" \r\n")
 			if pid.isdigit():
 				pid = int(pid)
