ef811d4a1f6bc61eeda98568c4ba4371b4a5f0da
Timothy Jannace <timjen3@gmail.com>
2017-09-03 14:49:29 -0400

    Stripped nl chars from pid file.

--- connect.py
+++ connect.py
@@ -49,18 +49,14 @@ def disconnect_function():
 def check_alive():
 	"""Checks if process defined in pid file is alive."""
 	# Added logger to see what's going on here on my linux machine. Seems to work, but can't tell pid is alive.
-	logger = logging.getLogger(__name__)
 	is_alive = False
 	if "pid_file" in tool_config and os.path.exists(tool_config["pid_file"]):
-		logger.info(tool_config["pid_file"])
 		with open(tool_config["pid_file"], "r") as fp:
-			pid = fp.read()
-			logger.info(pid)
+			pid = fp.read().strip(" \r\n")
 			if pid.isdigit():
 				pid = int(pid)
 			else:
 				pid = -1
-			logger.info("{} is type: {}".format(pid, type(pid)))
 			is_alive = pid_exists(pid)
 	locale_info = get_meta()
 	return is_alive, "IP:{}\nREGION:{}".format(locale_info.ip, locale_info.region)
