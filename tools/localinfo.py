315f678ad8db579f6dec94eab69ee99203b36d97
Timothy Jannace <timjen3@gmail.com>
2017-09-03 20:18:27 -0400

    logging

--- tools/localinfo.py
+++ tools/localinfo.py
@@ -6,12 +6,13 @@ from tools.http_connector import get_text
 def get_meta2():
 	"""Not as much data, but 1/2 api calls and 1/2 dependencies. Using this whenever possible. Still need the original
 	for purpose of Server ranking, but for any other checks this is adequate."""
-	# uuid4 doesn't seem to be working as expected on linux, so for now just using simple redirect.
-	# import uuid
-	# random_string = str(uuid.uuid4()).replace("-", "")
-	# ip_meta = get_json(url="http://{}.edns.ip-api.com/json".format(random_string))
-	ip_meta_url = "http://edns.ip-api.com/json"
-	ip_meta = get_json(url=ip_meta_url)
+	import logging
+	import uuid
+	random_string = str(uuid.uuid4()).replace("-", "")
+	meta_url = "http://{}.edns.ip-api.com/json".format(random_string)
+	logger = logging.getLogger(__name__)
+	logger.info(meta_url)
+	ip_meta = get_json(url=meta_url)
 	return LocaleInfo2(
 		dns_ip=ip_meta.get("dns", dict()).get("ip", ""),
 		dns_ip_geo=ip_meta.get("dns", dict()).get("geo", ""),
